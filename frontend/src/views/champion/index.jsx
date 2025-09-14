35e2c5596364ebb847159521f04cbed5239e0abe
Thao Nguyen <thao0210@gmail.com>
2025-09-14 16:50:31 +0700

    Auto deploy FE

--- frontend/src/views/champion/index.jsx
+++ frontend/src/views/champion/index.jsx
@@ -218,7 +218,7 @@ const Champions = () => {
           visible={true}
           points={totalPoints}
           content={returnContentHtml(myAward)}
-          time={1000}
+          time={5}
           onClose={onCongratsClose}
         />
       )}
